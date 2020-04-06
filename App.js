0b53dbe160d72317854ba631d64f40fbbf0a3f05
Jonno <jonnowelch@yahoo.com>
2020-04-06 16:36:31 +0100

    added a global font package

--- App.js
+++ App.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import Home from "./screens/home";
-// import { StyleSheet, Text, View } from "react-native";
+import * as Font from "expo-font";
+import { AppLoading } from "expo";
+
+const getFonts = () =>
+  Font.loadAsync({
+    "nunito-regular": require("./assets/fonts/Nunito-Regular.ttf"),
+    "nunito-bold": require("./assets/fonts/Nunito-Bold.ttf"),
+  });
 
 export default function App() {
-  return <Home></Home>;
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  if (fontsLoaded) {
+    return <Home />;
+  } else {
+    return (
+      <AppLoading startAsync={getFonts} onFinish={() => setFontsLoaded(true)} />
+    );
+  }
 }
