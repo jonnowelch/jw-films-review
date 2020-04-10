0de55fc614edfd20941cc104a117e35791d71864
Jonno <jonnowelch@yahoo.com>
2020-04-10 12:31:22 +0100

    turned data on homepage and review pages into card components

--- screens/home.js
+++ screens/home.js
@@ -1,21 +1,35 @@
-import React, { useState } from 'react';
-import { StyleSheet, View, Text, TouchableOpacity, FlatList } from 'react-native';
-import { globalStyles } from '../styles/global';
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  TouchableOpacity,
+  FlatList,
+} from "react-native";
+import { globalStyles } from "../styles/global";
+import Card from "../shared/card";
 
 export default function Home({ navigation }) {
   const [reviews, setReviews] = useState([
-    { title: 'Shawshank', rating: 5, body: 'lorem ipsum', key: '1' },
-    { title: 'Sin city', rating: 4, body: 'lorem ipsum', key: '2' },
-    { title: 'Avengers', rating: 3, body: 'lorem ipsum', key: '3' },
+    { title: "Shawshank", rating: 5, body: "lorem ipsum", key: "1" },
+    { title: "Sin city", rating: 4, body: "lorem ipsum", key: "2" },
+    { title: "Avengers", rating: 3, body: "lorem ipsum", key: "3" },
   ]);
 
   return (
     <View style={globalStyles.container}>
-      <FlatList data={reviews} renderItem={({ item }) => (
-        <TouchableOpacity onPress={() => navigation.navigate('ReviewDetails', item)}>
-          <Text style={globalStyles.titleText}>{ item.title }</Text>
-        </TouchableOpacity>
-      )} />
+      <FlatList
+        data={reviews}
+        renderItem={({ item }) => (
+          <TouchableOpacity
+            onPress={() => navigation.navigate("ReviewDetails", item)}
+          >
+            <Card>
+              <Text style={globalStyles.titleText}>{item.title}</Text>
+            </Card>
+          </TouchableOpacity>
+        )}
+      />
     </View>
   );
 }
--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { View, StyleSheet, Text, Button } from "react-native";
 import { globalStyles } from "../styles/global";
+import Card from "../shared/card";
 
 export default function ReviewDetails({ navigation }) {
   return (
     <View style={globalStyles.container}>
-      <Text> {navigation.getParam("title")}</Text>
-      <Text> {navigation.getParam("rating")}</Text>
-      <Text> {navigation.getParam("body")}</Text>
+      <Card>
+        <Text> {navigation.getParam("title")}</Text>
+        <Text> {navigation.getParam("rating")}</Text>
+        <Text> {navigation.getParam("body")}</Text>
+      </Card>
     </View>
   );
 }
