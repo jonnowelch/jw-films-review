6ce7784e19f70e819c24b98d1d941dcee86f85dc
Jonno <jonnowelch@yahoo.com>
2020-04-11 11:40:38 +0100

    added form valdiation with yup

--- screens/newReview.js
+++ screens/newReview.js
@@ -2,6 +2,18 @@ import React from "react";
 import { StyleSheet, Button, TextInput, View, Text } from "react-native";
 import { globalStyles } from "../styles/global.js";
 import { Formik } from "formik";
+import * as yup from "yup";
+
+const reviewSchema = yup.object({
+  title: yup.string().required().min(2),
+  body: yup.string().required().min(8),
+  rating: yup
+    .string()
+    .required()
+    .test("num1-5", "Rating is between 1 and 5", (val) => {
+      return parseInt(val) < 6 && parseInt(val) > 0;
+    }),
+});
 
 export default function ReviewForm({ addReview }) {
   return (
@@ -12,6 +24,7 @@ export default function ReviewForm({ addReview }) {
           body: "",
           rating: "",
         }}
+        validationSchema={reviewSchema}
         onSubmit={(values, actions) => {
           actions.resetForm();
           addReview(values);
